ba1516f332106ab73c15ffe189e386f664adfd99
dan360 <dan360@naver.com>
2025-02-11 10:25:42 +0900

    prototype의 소개

--- 250211/실습2/app.js
+++ 250211/실습2/app.js
@@ -1,17 +1,21 @@
-const dog1 = {
-  name: "바둑이",
-  age: 3,
+// Prototype 생성자 (constructor) 정의 구문
+// 일반 함수와 똑같이 생겼는데, this에 접근한다는 점이 다르다.
+// 대문자로 시작함
+function Dog(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-  bark: function () {
-    console.log(`${this.name} 멍멍!`);
-  },
+// 함수를 안쪽으로 집어넣을 수 없으니까,
+// 밖에 따로 정의해서 사용.
+// 들어갈 때 prototype을 사용.
+Dog.prototype.bark = function () {
+  console.log(`${this.name} 멍멍!`);
 };
 
-const dog2 = {
-  name: "몽실이",
-  age: 4,
-  bark: function () {
-    console.log(`${this.name} 멍멍!`);
-  },
-};
+const dog1 = new Dog("바둑이", 3);
+dog1.bark();
 
+dog1.ownerPhoneNumber = "010-6666-7777";
+
+// console.log(dog1);
