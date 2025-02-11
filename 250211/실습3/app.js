ba62a7245e6fdd61da7f24cd100e592861dcd840
dan360 <dan360@naver.com>
2025-02-11 10:51:45 +0900

    예제) 어벤저스 클래스 구현

--- 250211/실습3/app.js
+++ 250211/실습3/app.js
@@ -1,39 +1,40 @@
-class Dog {
-  constructor(name, age) {
-    this.name = name; // 강아지의 이름
-    this.age = age; // 강아지의 나이
-  }
+// class 문법으로 다음을 구현하라.
 
-  bark() {
-    console.log(`${this.name} 멍멍!`);
+// // 어벤져스 멤버를 나타내기 위한 클래스
+// Hero 클래스를 구현
+//   필드: name, age, skill, gender
+//   각각의 필드에 적합한 값을 constructor 를 사용해 배정하시오
+
+// // 다음 객체를 Hero 타입으로 생성
+// // ironman, hulk, captine
+
+// // 프로토타입의 원리에 따라, 나중에 배정해서 실행
+// ironman : shootLazor() : 출력: 레이저빔 빠방
+// hulk : smash() : 출력: 다 부숴버리겠다!
+// captine : niceIntroduce() : 출력: 반가워요 나는 캡틴입니다.
+
+class Hero {
+  constructor(name, age, skill, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
   }
 }
 
+const hero1 = new Hero("ironman", 20, "male");
+const hero2 = new Hero("hulk", 15, "male");
+const hero3 = new Hero("captine", 100, "male");
 
-// 사용 예시는 동일
-const dog1 = new Dog("바둑이", 3);
-const dog2 = new Dog("초코", 2);
+hero1.shootLazor = function () {
+  console.log(`레이저빔 빠방`);
+};
+hero2.smash = function () {
+  console.log(`다 부숴버리겠다!`);
+};
+hero3.niceIntroduce = function () {
+  console.log(`반가워요 나는 캡틴입니다.`);
+};
 
-console.log(dog1.name); // 바둑이
-console.log(dog1.age);  // 3
-
-console.log(dog2.name); // 초코
-console.log(dog2.age);  // 2
-
-dog1.bark(); // 바둑이 멍멍!
-dog2.bark(); // 초코 멍멍!
-
-// 특정 프로퍼티 추가도 가능
-dog1.ownerPhoneNumber = "010-6666-7777";
-dog2.eat = function () {
-  console.log("잘 먹겠습니다 주인님.");
-}
-
-// 전화번호 출력됨
-console.log(dog1.ownerPhoneNumber);
-// undefined
-console.log(dog2.ownerPhoneNumber);
-// error
-// console.log(dog1.eat());
-// 함수 실행됨
-console.log(dog2.eat());
+hero1.shootLazor();
+hero2.smash();
+hero3.niceIntroduce();
