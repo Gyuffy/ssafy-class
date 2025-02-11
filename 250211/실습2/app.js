2da4cc0fd3d0ee635a15b95627692bd39035ab0c
dan360 <dan360@naver.com>
2025-02-11 10:08:58 +0900

    실습2번 추가

---
+++ 250211/실습2/app.js
@@ -0,0 +1,17 @@
+const dog1 = {
+  name: "바둑이",
+  age: 3,
+
+  bark: function () {
+    console.log(`${this.name} 멍멍!`);
+  },
+};
+
+const dog2 = {
+  name: "몽실이",
+  age: 4,
+  bark: function () {
+    console.log(`${this.name} 멍멍!`);
+  },
+};
+
